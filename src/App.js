b300d39589ed3fb3d73d5b4565585ba18d105dae
K2302 <95227135+K2302@users.noreply.github.com>
2022-09-18 07:31:59 +0530

    ok

---
+++ src/App.js
@@ -0,0 +1,37 @@
+
+//import { Component } from 'react';
+import './App.css'
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  state = { advice: '' };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+    .then((response) => {
+      const { advice } = response.data.slip;
+
+      this.setState({ advice });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+   render(){ return (
+    <div className="app">
+    <div className="card">
+      <h1 className="heading">{this.state.advice}</h1>
+      <button className="button" onClick={this.fetchAdvice}>
+        <span id="ad">GIVE ME ADVICE!</span>
+      </button>
+    </div>
+  </div>
+  );
+}
+}
+
+export default App;
